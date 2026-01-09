2875554aae160eda415ca3b7c4af22bf18961f9b
gmf-uah <gmf0010@uah.edu>
2026-01-08 21:58:50 -0600

    Move JavaScript code from index.html to script.js

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-// The content of your JavaScript logic goes here
-// Assuming your JavaScript logic looks similar to this:
-document.querySelector('form').addEventListener('submit', function(event) {
-    // event.preventDefault(); // Commented out for the task
-    alert('Form submitted!');
-});
+// Migrate JavaScript content from index.html
+console.log('Hello, World!');
