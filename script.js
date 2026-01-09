d7f5356717f13ffd5c833e980a5f57dc475ff15d
gmf-uah <gmf0010@uah.edu>
2026-01-08 21:54:42 -0600

    Extract JavaScript logic from index.html into script.js and comment out preventDefault.

---
+++ script.js
@@ -0,0 +1,6 @@
+// The content of your JavaScript logic goes here
+// Assuming your JavaScript logic looks similar to this:
+document.querySelector('form').addEventListener('submit', function(event) {
+    // event.preventDefault(); // Commented out for the task
+    alert('Form submitted!');
+});
